d9753e5679f0dfdc0fd053c412a148fe16ebfa90
xuxichen <xuxichen@xuxichen>
2018-09-16 20:48:18 +0800

    init project

---
+++ src/main/java/com/xuxc/loginModule/LoginModuleApplication.java
@@ -0,0 +1,12 @@
+package com.xuxc.loginModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginModuleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xuxc/loginModule/LoginModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xuxc.loginModule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginModuleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
